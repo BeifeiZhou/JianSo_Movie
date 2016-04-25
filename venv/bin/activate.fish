d8a54150c5c88e84939a03002d36137bf713986a
but0n <but0n@freeattack.com>
2016-04-25 22:16:31 +0800

    mv development to localhost

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/fs_work/venv"
+set -gx VIRTUAL_ENV "/Users/but0n/Desktop/flask_test/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
