5dd756d80d66dd29a1c3728880dc62d64aaece4c
but0n <but0n@freeattack.com>
2016-04-26 01:16:02 +0800

    route

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def Hello():
-	return 'hello but0n, comming soon!'
+	render_template('index.html')
 @app.route('/log/<int:id>')
 def log(id):
 	if id == 2704:
