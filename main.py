27fe5ae0e286d60ab3e676106f9f2496bd04d00f
but0n <jack32700567@icloud.com>
2016-04-25 18:37:25 +0800

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def Hello():
+	return 'hello but0n, comming soon!'
+@app.route('/log/<int:id>')
+def log(id):
+	if id == 2704:
+		return 's'
+	else:
+		return 'erro :)'
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port = 80)
