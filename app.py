6c2c9c741a083a6ea3b194b8aeca5519b6b67848
but0n <but0n@freeattack.com>
2016-05-08 02:57:08 +0800

    还有referer未修复

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ def search(key):
 	cur = g.db.cursor()
 	cur.execute('SELECT * FROM movies WHERE name LIKE ?',('%'+key+'%',))
 	dat = cur.fetchall()
-	return render_template('searchList.html')
-	# return render_template('searchList.html', title = key, result_len = len(dat), result = dat)
+	return render_template('searchList.html', title = key, result_len = len(dat), result = dat)
 
 
 if __name__ == '__main__':
